2e5a294a4c763c11fe52193b8763d8e0a76510ea
Samir <srajkarnikar9@gmail.com>
2025-03-04 19:15:10 +0545

    added image gallery with modal and tictactoe game

--- Task - 26-2-2025/main.js
+++ Task - 26-2-2025/main.js
@@ -95,5 +95,129 @@ function addTask(task) {
 
 // also on the expanded image, create a cross bottom on top left to go to default gallery view
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get modal elements
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+  var captionText = document.getElementById("caption");
+
+  // Select all images inside .image and add click event listener
+  var images = document.querySelectorAll(".image img");
+
+  images.forEach(function (img) {
+      img.addEventListener("click", function () {
+          modal.style.display = "flex"; 
+          modalImg.src = this.src; 
+          captionText.innerHTML = this.alt; 
+      });
+  });
+
+  // Close the modal when clicking the close button
+  document.querySelector(".close").addEventListener("click", function () {
+      modal.style.display = "none";
+  });
+});
+
 // 3. Create a dynamic TIC TAC TOE game
-// URL:https://codepen.io/Samir-Rajkarnikar/pen/VYZKXKP
+let boxes = document.querySelectorAll(".box");
+let resetGame = document.querySelector("#reset");
+let player = document.querySelector("#player");
+let notify = document.querySelector("#turn");
+let newGame = document.querySelector("#newgame");
+let results = document.querySelector(".results");
+let msg = document.querySelector(".message");
+
+let turnO = true; //player O goes first
+let clickCount = 0;
+
+const winPatters = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      player.innerText = `X`;
+      turnO = false;
+      box.disabled = true;
+      clickCount++;
+      if(!box.classList.contains('red')){
+        box.classList.add('red');
+      }
+    } 
+    else {
+      box.innerText = "X";
+      player.innerText = `O`;
+      box.disabled = true;
+      turnO = true;
+      if(!box.classList.contains('black')){
+        box.classList.add('black');
+      }
+    }
+    box.disaled = true;
+    checkWinner();
+    if (clickCount == 9) {
+      msg.innerText = `Game has ended in draw.`;
+      results.classList.remove("hide");
+      notify.classList.add("hide");
+      resetGame.classList.add("hide");
+      disableBoxes();
+    }
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winPatters) {
+    let posVal1 = boxes[pattern[0]].innerText;
+    let posVal2 = boxes[pattern[1]].innerText;
+    let posVal3 = boxes[pattern[2]].innerText;
+
+    if (posVal1 !== "" && posVal2 !== "" && posVal3 !== "") {
+      if (posVal1 === posVal2 && posVal2 === posVal3) {
+        winMessage(posVal1);
+      }
+    }
+  }
+};
+
+const winMessage = (winner) => {
+  msg.innerText = `Congratulations, ${winner} is the winner`;
+  results.classList.remove("hide");
+  disableBoxes();
+  notify.classList.add("hide");
+  resetGame.classList.add("hide");
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true; 
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+    box.classList.remove('red', 'black');
+  }
+};
+
+const reset = () => {
+  turnO = true;
+  enableBoxes();
+  results.classList.add("hide");
+  clickCount = 0;
+  resetGame.classList.remove("hide");
+  notify.classList.remove("hide");
+};
+
+newGame.addEventListener("click", reset);
+resetGame.addEventListener("click", reset);
